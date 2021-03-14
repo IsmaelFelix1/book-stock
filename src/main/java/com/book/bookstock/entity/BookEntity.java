719b2ea1c7ee4cb2cb52deccfde5a1b4f6bd71db
Ismael Felix <ismael.duarte017@gmail.com>
2021-03-14 17:41:49 -0300

    implementation in the book entity class

--- src/main/java/com/book/bookstock/entity/BookEntity.java
+++ src/main/java/com/book/bookstock/entity/BookEntity.java
@@ -1,8 +1,35 @@
 package com.book.bookstock.entity;
 
+import com.book.bookstock.enums.BookTypeEnum;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private Integer max;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookTypeEnum type;
 }
